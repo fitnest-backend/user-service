7dcea53831a39fecfd94245bf98ff49fd87c5f19
zarifaalili <alilizarifa@gmail.com>
2026-01-25 20:52:12 +0400

    refac: some changes were added

--- src/main/java/az/fitnest/userservice/UserServiceApplication.java
+++ src/main/java/az/fitnest/userservice/UserServiceApplication.java
@@ -1,10 +1,5 @@
 package az.fitnest.userservice;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
