851d03e1329440da34fd681d9760ff005a177eb3
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-14 13:13:30 +0400

    improved swagger explanation, detail, dto explanation and so more on

--- src/main/java/az/fitnest/user/controller/GoalReferenceController.java
+++ src/main/java/az/fitnest/user/controller/GoalReferenceController.java
@@ -8,6 +8,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
