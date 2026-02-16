df518181ad6b5588beb4bb961eba5fb2f3c95322
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-16 12:15:45 +0400

    duplicate header token

--- src/main/java/az/fitnest/user/client/TeraBoxWorkerFeignConfig.java
+++ src/main/java/az/fitnest/user/client/TeraBoxWorkerFeignConfig.java
@@ -2,7 +2,6 @@ package az.fitnest.user.client;
 
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 public class TeraBoxWorkerFeignConfig {
 
