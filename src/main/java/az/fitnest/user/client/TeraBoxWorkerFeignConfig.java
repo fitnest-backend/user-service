ab92476a6cbae257839e066131ce9fdbbfa12ab1
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-16 11:59:41 +0400

    fixed download url

--- src/main/java/az/fitnest/user/client/TeraBoxWorkerFeignConfig.java
+++ src/main/java/az/fitnest/user/client/TeraBoxWorkerFeignConfig.java
@@ -9,8 +9,14 @@ public class TeraBoxWorkerFeignConfig {
     @Bean
     public RequestInterceptor teraBoxRequestInterceptor() {
         return requestTemplate -> {
+            System.out.println("TeraBoxWorkerFeignConfig: Injecting X-Internal-Token header");
             requestTemplate.removeHeader("Authorization");
             requestTemplate.header("X-Internal-Token", "shared-secret-token");
         };
+    }
+
+    @Bean
+    feign.Logger.Level feignLoggerLevel() {
+        return feign.Logger.Level.FULL;
     }
 }
