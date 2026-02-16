fa3bd62a46ec4cd0fe17257bd2b8657e50bca32d
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-16 10:59:58 +0400

    fix resource + hpa

--- src/main/java/az/fitnest/user/client/TeraBoxWorkerFeignConfig.java
+++ src/main/java/az/fitnest/user/client/TeraBoxWorkerFeignConfig.java
@@ -4,7 +4,6 @@ import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class TeraBoxWorkerFeignConfig {
 
     @Bean
