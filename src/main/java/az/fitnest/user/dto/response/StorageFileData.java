6ff36a473c2d37fa51cd213670ac4a369546cff1
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-02-19 11:47:00 +0400

    fix

---
+++ src/main/java/az/fitnest/user/dto/response/StorageFileData.java
@@ -0,0 +1,11 @@
+package az.fitnest.user.dto.response;
+
+import lombok.Data;
+
+@Data
+public class StorageFileData {
+    private String path;
+    private long size;
+    private String md5;
+    private long fs_id;
+}
