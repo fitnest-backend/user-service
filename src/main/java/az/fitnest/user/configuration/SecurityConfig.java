a93cd00efe7f3b7044cce9e476cb85ff03b306e9
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-09 20:19:15 +0400

    fix: remove unnecessary blank line in SecurityConfig constructor

--- src/main/java/az/fitnest/user/configuration/SecurityConfig.java
+++ src/main/java/az/fitnest/user/configuration/SecurityConfig.java
@@ -21,7 +21,6 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 @EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
-
     private final FitnestSecurityFilter securityFilter;
 
     @Bean
