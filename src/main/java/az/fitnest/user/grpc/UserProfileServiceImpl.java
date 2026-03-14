b6d3721158ba6d203af04ec45f8e11e23cd4517b
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-15 01:47:23 +0400

    fix: specify service name for UserProfileServiceImpl to enable gRPC bean injection

--- src/main/java/az/fitnest/user/grpc/UserProfileServiceImpl.java
+++ src/main/java/az/fitnest/user/grpc/UserProfileServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import java.time.format.DateTimeFormatter;
 
-@Service
+@Service("grpcUserProfileServiceImpl")
 public class UserProfileServiceImpl extends UserProfileServiceGrpc.UserProfileServiceImplBase {
     @Autowired
     private UserProfileRepository userProfileRepository;
