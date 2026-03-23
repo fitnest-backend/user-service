0ae8cc83faab987ccee9c7b475760137c11cc364
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-03-23 13:12:30 +0400

    chore: add JsonInclude annotation to GoalItemResponse to always include fields in JSON output

--- src/main/java/az/fitnest/user/dto/response/GoalItemResponse.java
+++ src/main/java/az/fitnest/user/dto/response/GoalItemResponse.java
@@ -1,8 +1,10 @@
 package az.fitnest.user.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 
 @Builder
+@JsonInclude(JsonInclude.Include.ALWAYS)
 public record GoalItemResponse(
     String code,
     String title,
