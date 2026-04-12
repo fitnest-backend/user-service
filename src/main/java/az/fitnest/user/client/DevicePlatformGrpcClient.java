f4c8487e053a5ee5bb737497cd54bde7f60b0075
Kamal Pashayev <infokamalpashayev@gmail.com>
2026-04-12 21:23:55 +0400

    chore: update service names in gRPC interceptors and client for consistency

--- src/main/java/az/fitnest/user/client/DevicePlatformGrpcClient.java
+++ src/main/java/az/fitnest/user/client/DevicePlatformGrpcClient.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class DevicePlatformGrpcClient {
     public String getUserPlatform(Long userId) {
-        ManagedChannel channel = ManagedChannelBuilder.forAddress("notifications-service", 9091)
+        ManagedChannel channel = ManagedChannelBuilder.forAddress("notifications-backend", 9091)
                 .usePlaintext()
                 .build();
         DevicePlatformServiceGrpc.DevicePlatformServiceBlockingStub stub = DevicePlatformServiceGrpc.newBlockingStub(channel);
--- src/main/java/az/fitnest/user/security/PatternAGrpcInterceptor.java
+++ src/main/java/az/fitnest/user/security/PatternAGrpcInterceptor.java
@@ -72,7 +72,7 @@ public class PatternAGrpcInterceptor implements ServerInterceptor, ClientInterce
                     propagate(request, headers, "X-Scopes", X_SCOPES);
                     propagate(request, headers, "X-Request-Id", X_REQUEST_ID);
                     propagate(request, headers, "X-From-Gateway", X_FROM_GATEWAY);
-                    headers.put(X_SERVICE_NAME, "user-service");
+                    headers.put(X_SERVICE_NAME, "user-backend");
                 }
                 super.start(responseListener, headers);
             }
