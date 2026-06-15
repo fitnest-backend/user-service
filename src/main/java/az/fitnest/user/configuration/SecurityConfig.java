93f124c977356d1b5e4656756712f27a5935cde7
nijataghayev <nicat.agayev.536@gmail.com>
2026-06-15 15:30:31 +0400

    /api/v1/me/profile/images/** permitAll

--- src/main/java/az/fitnest/user/configuration/SecurityConfig.java
+++ src/main/java/az/fitnest/user/configuration/SecurityConfig.java
@@ -39,6 +39,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/bmi/**").permitAll()
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
                         .requestMatchers("/api/v1/goals/images/**").permitAll()
+                        .requestMatchers("/api/v1/me/profile/images/**").permitAll()
                         .requestMatchers("/actuator/**", "/health/**").permitAll()
                         .requestMatchers("/error").permitAll()
                         .anyRequest().authenticated()
